c095f0afe3b2ee4f60647e791cf648daa8ea84ed
Sifat <105747804+sifat371@users.noreply.github.com>
2026-02-24 09:00:43 +0600

    Refactor MNIST model to use CNN with Fashion MNIST

--- mnist.py
+++ mnist.py
@@ -1,89 +1,88 @@
-from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten
+from tensorflow.keras.optimizers import Adagrad
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.datasets import fashion_mnist
 import numpy as np
-import matplotlib.pyplot as plt
+
+(trainX, trainY), (testX, testY) = fashion_mnist.load_data()
+
+trainX = trainX.astype('float32') / 255.0
+testX = testX.astype('float32') / 255.0
+
+trainX = trainX.reshape(-1, 28, 28, 1)
+testX = testX.reshape(-1, 28, 28, 1)
+
+trainY = to_categorical(trainY, 10)
+testY = to_categorical(testY, 10)
+
+total = len(trainX)
+train_size = int(0.85 * total)
+
+x_train = trainX[:train_size]
+y_train = trainY[:train_size]
+
+x_val = trainX[train_size:]
+y_val = trainY[train_size:]
 
 
-def polimonial(x):
-    return 7*x**4 - 4*x**3 - x + 6
+inputs = Input((28, 28, 1))
 
-def data_processing():
-    n = 10000
+x = Conv2D(16, (3,3), activation='relu', name='conv1')(inputs)
+x = Conv2D(32, (3,3), activation='relu', name='conv2')(x)
+x = Conv2D(64, (3,3), activation='relu', name='conv3')(x)
 
-    x = np.random.uniform(-15, 15, n)
-    y = polimonial(x)
+x = Flatten()(x)
+x = Dense(128, activation='relu')(x)
+x = Dense(32, activation='relu')(x)
+outputs = Dense(10, activation='softmax')(x)   # ⭐ 10 classes
 
-    x_min, x_max = x.min(), x.max()
-    #x_scaled = (x - x_min)/(x_max - x_min)
+model = Model(inputs, outputs)
+model.summary(show_trainable=True)
 
-    y_min, y_max = y.min(), y.max()
-    #y_scaled = (y - y_min)/(y_max - y_min)
-
-    x_scaled = 2 * (x - x_min) / (x_max - x_min) - 1
-    y_scaled = 2 * (y - y_min) / (y_max - y_min) - 1
-
-    return x_scaled, y_scaled, x_max, y_max, x_min, y_min
-
-def data_prepatation():
-    x_scaled, y_scaled, x_max, y_max, x_min, y_min = data_processing()
-    total_n = len(x_scaled)
-    train_n = int(total_n * 0.8)
-    val_n = int(total_n* 0.1)
-
-    train_x = x_scaled[:train_n].reshape(-1, 1)
-    train_y = y_scaled[:train_n]
-
-    val_x = x_scaled[train_n: train_n+val_n].reshape(-1,1)
-    val_y = y_scaled[train_n: train_n+val_n]
-
-    test_x = x_scaled[train_n+val_n:].reshape(-1,1)
-    test_y = y_scaled[train_n+val_n:]
-
-    return (train_x, train_y), (val_x, val_y), (test_x, test_y)
-
-def build_model():
-    inputs = Input((1,))
-    h1 = Dense(32, activation="relu")(inputs)
-    h2 = Dense(64, activation="relu")(h1)
-    h3 = Dense(128, activation="relu")(h2)
-    outputs = Dense(1)(h3)
-    model = Model(inputs, outputs)
-    model.summary(show_trainable = True)
-    return model
-
-def main():
-    model = build_model()
-    model.compile(loss = 'mse', optimizer = Adam(learning_rate = 1e-4))
-
-    (train_x, train_y), (val_x, val_y), (test_x, test_y) = data_prepatation()
-    
-    history = model.fit(
-        train_x,
-        train_y,
-        validation_data = (val_x, val_y),
-        epochs = 10,
-        verbose = 1,
-        batch_size = 32
-    )
-    y_test_prediction = model.predict(test_x)
-
-    # # plt.plot(test_x, y_test_prediction, color = 'red', label = 'predict')
-    # # plt.plot(test_y, test_y, color = 'blue', label = 'ideal')
-    plt.scatter(test_x, test_y, label="True")
-    plt.scatter(test_x, y_test_prediction, label="Predicted")
-
-    # model.evaluate(test_x, test_y)
-    plt.grid()
-    plt.legend()
-    plt.show()
-
-    plt.plot(history.history['loss'], label='Train MSE')
-    plt.plot(history.history['val_loss'], label='Val MSE')
-    plt.legend()
-    plt.grid()
-    plt.show()
+checkpoint = ModelCheckpoint(
+    'best_model.keras',
+    monitor='val_loss',
+    save_best_only=True,
+    verbose=1
+)
 
 
-if __name__ == "__main__":
-    main()
+model.compile(optimizer=Adagrad(learning_rate=0.003),
+              loss="categorical_crossentropy",
+              metrics=['accuracy'])
+
+history1 = model.fit(x_train, y_train,
+          validation_data=(x_val, y_val),
+          epochs=2,
+          batch_size=32,
+          callbacks=[checkpoint])
+
+for layer in model.layers:
+    if 'conv' in layer.name:
+        layer.trainable = False
+
+model.compile(optimizer=Adagrad(learning_rate=0.003),
+              loss="categorical_crossentropy",
+              metrics=['accuracy'])
+
+history2 = model.fit(x_train, y_train,
+          validation_data=(x_val, y_val),
+          epochs=3,
+          batch_size=32,
+          callbacks=[checkpoint])
+
+test_loss, test_acc = model.evaluate(testX, testY)
+print("Test Accuracy :", test_acc)
+print("Test Loss :", test_loss)
+
+
+loss = history1.history['loss'] + history2.history['loss']
+val_loss = history1.history['val_loss'] + history2.history['val_loss']
+
+plt.plot(loss, label='Train Loss')
+plt.plot(val_loss, label='Val Loss')
+plt.legend()
+plt.grid()
+plt.show()
